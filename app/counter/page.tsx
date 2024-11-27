47cdf8ab1d882e3c4313b78f77785f19817f755d
daniel.ropot <daniel.ropot@hmmh.de>
2024-11-27 18:58:21 +0100

    first commit

---
+++ app/counter/page.tsx
@@ -0,0 +1,11 @@
+import Counter from "@/components/Counter";
+
+const CounterPage = () => {
+  return (
+    <section>
+      <h1 className="text-6xl mb-16">Page content</h1>
+      <Counter />
+    </section>
+  );
+};
+export default CounterPage;
---
+++ app/tours/[id]/page.tsx
@@ -0,0 +1,8 @@
+const page = ({ params }: { params: { id: string } }) => {
+  return (
+    <div>
+      <h1 className="text-4xl">ID : {params.id}</h1>
+    </div>
+  );
+};
+export default page;
